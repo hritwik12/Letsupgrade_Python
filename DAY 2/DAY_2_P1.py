f7039c8b28856ac4d38464754f4709feb5ff89f2
Hritwik Jain <54503027+hritwik12@users.noreply.github.com>
2021-08-01 21:14:00 +0530

    Add files via upload

---
+++ DAY 2/DAY_2_P1.py
@@ -0,0 +1,12 @@
+def ispangram(str):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in alphabet:
+        if char not in str.lower():
+            return False
+  
+    return True
+a =input()
+if(ispangram(a) == True):
+    print("Yes")
+else:
+    print("No")
